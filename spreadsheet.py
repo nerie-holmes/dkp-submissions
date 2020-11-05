a571b8ff2114f563516090d1ad821c01c1828dd6
nerie <nerie.holmes@gmail.com>
2020-11-06 01:10:58 +0200

    writing into sheet

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,15 @@
 import os
 import json
 import gspread
+import urllib
+import pandas
 from oauth2client.service_account import ServiceAccountCredentials
+
+
+url = "https://kc.kobotoolbox.org/api/v1/data/546740?format=json"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+df = pd.json_normalize(data)
 
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
@@ -17,8 +25,10 @@ client = gspread.authorize(creds)
 
 # Find sheet by url
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1h67jenbOxA9a8g4M2ow3OHCabA_4D1oGCNg-4ic7a8A/edit?usp=sharing")
-sheet = spreadsheet.sheet1
+sheet = spreadsheet.sheet2
+
+sheet.update([df.columns.values.tolist()] + df.values.tolist())
 
 # Extract and print all of the values
-rows = sheet.get_all_records()
-print(rows)
+#rows = sheet.get_all_records()
+#print(rows)
